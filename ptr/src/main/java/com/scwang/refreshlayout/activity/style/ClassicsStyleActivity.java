7e94dea6d0a82a3c8fb95a2f12f660ce26e175e8
李习靖 <22126153+APlxj@users.noreply.github.com>
2018-04-23 10:54:26 +0800

    test

--- ptr/src/main/java/com/scwang/refreshlayout/activity/style/ClassicsStyleActivity.java
+++ ptr/src/main/java/com/scwang/refreshlayout/activity/style/ClassicsStyleActivity.java
@@ -26,9 +26,9 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
-public class ClassicsStyleActivity extends BaseActivity implements AdapterView.OnItemClickListener {
+public class ClassicsStyleActivity extends BaseActivity /*implements AdapterView.OnItemClickListener*/ {
 
-    private BaseRecyclerAdapter<Item> mAdpater;
+    /*private BaseRecyclerAdapter<Item> mAdpater;
 
     private enum Item {
         尺寸拉伸("下拉的时候Header的高度跟随变大"),
@@ -47,24 +47,18 @@ public class ClassicsStyleActivity extends BaseActivity implements AdapterView.O
         Item(String name) {
             this.name = name;
         }
-    }
+    }*/
 
-    private Toolbar mToolbar;
-    private RecyclerView mRecyclerView;
-    private RefreshLayout mRefreshLayout;
-    private ClassicsHeader mClassicsHeader;
-    private Drawable mDrawableProgress;
     private static boolean isFirstEnter = true;
-    private ClassicsFooter classicsFooter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_style_classics);
 
-        classicsFooter = (ClassicsFooter) findViewById(R.id.ClassicsFooter);
+        ClassicsFooter classicsFooter = (ClassicsFooter) findViewById(R.id.ClassicsFooter);
         classicsFooter.setIRefresh(this);
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,17 +66,18 @@ public class ClassicsStyleActivity extends BaseActivity implements AdapterView.O
             }
         });
 
-        mRefreshLayout = (RefreshLayout) findViewById(R.id.refreshLayout);
+        RefreshLayout mRefreshLayout = (RefreshLayout) findViewById(R.id.refreshLayout);
 
         int deta = new Random().nextInt(7 * 24 * 60 * 60 * 1000);
-        mClassicsHeader = (ClassicsHeader) mRefreshLayout.getRefreshHeader();
-        mClassicsHeader.setLastUpdateTime(new Date(System.currentTimeMillis() - deta));
-        mClassicsHeader.setTimeFormat(new SimpleDateFormat("更新于 MM-dd HH:mm", Locale.CHINA));
-        mClassicsHeader.setTimeFormat(new DynamicTimeFormat("更新于 %s"));
-
-        mDrawableProgress = mClassicsHeader.getProgressView().getDrawable();
-        if (mDrawableProgress instanceof LayerDrawable) {
-            mDrawableProgress = ((LayerDrawable) mDrawableProgress).getDrawable(0);
+        ClassicsHeader mClassicsHeader = (ClassicsHeader) mRefreshLayout.getRefreshHeader();
+        if (mClassicsHeader != null) {
+            mClassicsHeader.setLastUpdateTime(new Date(System.currentTimeMillis() - deta));
+            mClassicsHeader.setTimeFormat(new SimpleDateFormat("更新于 MM-dd HH:mm", Locale.CHINA));
+            mClassicsHeader.setTimeFormat(new DynamicTimeFormat("更新于 %s"));
+            /*Drawable mDrawableProgress = mClassicsHeader.getProgressView().getDrawable();
+            if (mDrawableProgress instanceof LayerDrawable) {
+                mDrawableProgress = ((LayerDrawable) mDrawableProgress).getDrawable(0);
+            }*/
         }
 
         /*View view = findViewById(R.id.recyclerView);
@@ -116,74 +111,72 @@ public class ClassicsStyleActivity extends BaseActivity implements AdapterView.O
         Toast.makeText(this, "加载完成", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        switch (Item.values()[position]) {
-            case 背后固定:
-                mClassicsHeader.setSpinnerStyle(SpinnerStyle.FixedBehind);
-                mRefreshLayout.setPrimaryColors(0xff444444, 0xffffffff);
-                if (Build.VERSION.SDK_INT >= 21) {
-                    mDrawableProgress.setTint(0xffffffff);
-                } else if (mDrawableProgress instanceof VectorDrawableCompat) {
-                    ((VectorDrawableCompat) mDrawableProgress).setTint(0xffffffff);
-                }
-                /*
-                 * 由于是后面才设置，需要手动更改视图的位置
-                 * 如果在 onCreate 或者 xml 中设置好[SpinnerStyle] 就不用手动调整位置了
-                 */
-                mRefreshLayout.getLayout().bringChildToFront(mRecyclerView);
-                break;
-            case 尺寸拉伸:
-                mClassicsHeader.setSpinnerStyle(SpinnerStyle.Scale);
-                break;
-            case 位置平移:
-                mClassicsHeader.setSpinnerStyle(SpinnerStyle.Translate);
-                break;
-            case 显示时间:
-                mClassicsHeader.setEnableLastTime(true);
-                break;
-            case 隐藏时间:
-                mClassicsHeader.setEnableLastTime(false);
-                break;
-            case 默认主题:
-                setThemeColor(R.color.colorPrimary, R.color.colorPrimaryDark);
-                mRefreshLayout.getLayout().setBackgroundResource(android.R.color.transparent);
-                mRefreshLayout.setPrimaryColors(0, 0xff666666);
-                if (Build.VERSION.SDK_INT >= 21) {
-                    mDrawableProgress.setTint(0xff666666);
-                } else if (mDrawableProgress instanceof VectorDrawableCompat) {
-                    ((VectorDrawableCompat) mDrawableProgress).setTint(0xff666666);
-                }
-                break;
-            case 蓝色主题:
-                setThemeColor(R.color.colorPrimary, R.color.colorPrimaryDark);
-                break;
-            case 绿色主题:
-                setThemeColor(android.R.color.holo_green_light, android.R.color.holo_green_dark);
-                break;
-            case 红色主题:
-                setThemeColor(android.R.color.holo_red_light, android.R.color.holo_red_dark);
-                break;
-            case 橙色主题:
-                setThemeColor(android.R.color.holo_orange_light, android.R.color.holo_orange_dark);
-                break;
-            case 加载更多:
-                mRefreshLayout.autoLoadmore();
-                return;
-        }
-        mRefreshLayout.autoRefresh();
-    }
-
-    private void setThemeColor(int colorPrimary, int colorPrimaryDark) {
-        mToolbar.setBackgroundResource(colorPrimary);
-        mRefreshLayout.setPrimaryColorsId(colorPrimary, android.R.color.white);
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().setStatusBarColor(ContextCompat.getColor(this, colorPrimaryDark));
-            mDrawableProgress.setTint(0xffffffff);
-        } else if (mDrawableProgress instanceof VectorDrawableCompat) {
-            ((VectorDrawableCompat) mDrawableProgress).setTint(0xffffffff);
-        }
-    }
-
-
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        switch (Item.values()[position]) {
+//            case 背后固定:
+//                mClassicsHeader.setSpinnerStyle(SpinnerStyle.FixedBehind);
+//                mRefreshLayout.setPrimaryColors(0xff444444, 0xffffffff);
+//                if (Build.VERSION.SDK_INT >= 21) {
+//                    mDrawableProgress.setTint(0xffffffff);
+//                } else if (mDrawableProgress instanceof VectorDrawableCompat) {
+//                    ((VectorDrawableCompat) mDrawableProgress).setTint(0xffffffff);
+//                }
+//                /*
+//                 * 由于是后面才设置，需要手动更改视图的位置
+//                 * 如果在 onCreate 或者 xml 中设置好[SpinnerStyle] 就不用手动调整位置了
+//                 */
+//                mRefreshLayout.getLayout().bringChildToFront(mRecyclerView);
+//                break;
+//            case 尺寸拉伸:
+//                mClassicsHeader.setSpinnerStyle(SpinnerStyle.Scale);
+//                break;
+//            case 位置平移:
+//                mClassicsHeader.setSpinnerStyle(SpinnerStyle.Translate);
+//                break;
+//            case 显示时间:
+//                mClassicsHeader.setEnableLastTime(true);
+//                break;
+//            case 隐藏时间:
+//                mClassicsHeader.setEnableLastTime(false);
+//                break;
+//            case 默认主题:
+//                setThemeColor(R.color.colorPrimary, R.color.colorPrimaryDark);
+//                mRefreshLayout.getLayout().setBackgroundResource(android.R.color.transparent);
+//                mRefreshLayout.setPrimaryColors(0, 0xff666666);
+//                if (Build.VERSION.SDK_INT >= 21) {
+//                    mDrawableProgress.setTint(0xff666666);
+//                } else if (mDrawableProgress instanceof VectorDrawableCompat) {
+//                    ((VectorDrawableCompat) mDrawableProgress).setTint(0xff666666);
+//                }
+//                break;
+//            case 蓝色主题:
+//                setThemeColor(R.color.colorPrimary, R.color.colorPrimaryDark);
+//                break;
+//            case 绿色主题:
+//                setThemeColor(android.R.color.holo_green_light, android.R.color.holo_green_dark);
+//                break;
+//            case 红色主题:
+//                setThemeColor(android.R.color.holo_red_light, android.R.color.holo_red_dark);
+//                break;
+//            case 橙色主题:
+//                setThemeColor(android.R.color.holo_orange_light, android.R.color.holo_orange_dark);
+//                break;
+//            case 加载更多:
+//                mRefreshLayout.autoLoadmore();
+//                return;
+//        }
+//        mRefreshLayout.autoRefresh();
+//    }
+//
+//    private void setThemeColor(int colorPrimary, int colorPrimaryDark) {
+//        mToolbar.setBackgroundResource(colorPrimary);
+//        mRefreshLayout.setPrimaryColorsId(colorPrimary, android.R.color.white);
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            getWindow().setStatusBarColor(ContextCompat.getColor(this, colorPrimaryDark));
+//            mDrawableProgress.setTint(0xffffffff);
+//        } else if (mDrawableProgress instanceof VectorDrawableCompat) {
+//            ((VectorDrawableCompat) mDrawableProgress).setTint(0xffffffff);
+//        }
+//    }
 }
